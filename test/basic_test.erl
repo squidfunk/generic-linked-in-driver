d89382574f63cadefca1c914ba5ab08471f8c926
Tom Janssens <Tom@corebvba.be>
2013-10-04 23:17:10 -0700

    Setup and teardown gives me a segmentation fault

--- test/basic_test.erl
+++ test/basic_test.erl
@@ -2,10 +2,15 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+setup_and_teardown_test()->
+  {ok, Drv} = gen_driver_test:start_link(),
+  gen_driver_test:stop(Drv).
+
 basic_test() ->
-  gen_driver_test:start_link(),
+  {ok, Drv} = gen_driver_test:start_link(),
   timer:sleep(500),
-  ?assertEqual(100,gen_driver_test:sum(1,2,97)),
-  gen_driver_test:stop().
+  {ok, Result} = gen_driver_test:sum(Drv,[1,2,97]), 
+  ?assertEqual(100.0, Result),
+  gen_driver_test:stop(Drv).
 
 
